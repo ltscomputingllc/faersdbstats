5e0127781420b74cca4e403339327c1aa6236886
leeevans <levans@ltscomputingllc.com>
2015-09-06 08:37:19 -0400

    derive standard case indications across all unique legacy and current cases

--- standardized_output/derive_standard_case_indication.sql
+++ standardized_output/derive_standard_case_indication.sql
@@ -1,6 +1,6 @@
 ------------------------------
 --
--- This SQL script converts the FAERS indication MedDRA preferred terms into MedDRA concept ids (limited to the cases in the unique_case table) 
+-- This SQL script converts the indication MedDRA preferred terms into MedDRA concept ids for all unique legacy and current cases
 -- in a new table called standard_case_indication 
 -- Note. We use a regex to remove leading white space in the indication preferred term field
 --
@@ -11,10 +11,20 @@ set search_path = faers;
 
 drop table if exists standard_case_indication;
 create table standard_case_indication as
-select distinct a.primaryid, b.indi_pt, c.concept_id as indication_concept_id
-from unique_case a
+select distinct a.primaryid, a.isr, b.indi_pt, c.concept_id as indication_concept_id
+from unique_all_case a
 inner join indi b
 on a.primaryid = b.primaryid
 inner join cdmv5.concept c
 on upper(regexp_replace(b.indi_pt,'^ +','','gi')) = upper(c.concept_name) 
-and c.vocabulary_id = 'MedDRA'; 
+and c.vocabulary_id = 'MedDRA'
+where a.isr is null
+union all
+select distinct a.primaryid, a.isr, b.indi_pt, c.concept_id as indication_concept_id
+from unique_all_case a
+inner join indi_legacy b
+on a.isr = b.isr
+inner join cdmv5.concept c
+on upper(regexp_replace(b.indi_pt,'^ +','','gi')) = upper(c.concept_name) 
+and c.vocabulary_id = 'MedDRA'
+where a.isr is not null;
