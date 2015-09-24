62724047c0768f747bc0f2f1b78e540dddadc09a
leeevans <levans@ltscomputingllc.com>
2015-09-24 11:09:27 -0400

    round the statistics to five decimal places

--- standardized_output/derive_standard_drug_outcome_statistics.sql
+++ standardized_output/derive_standard_drug_outcome_statistics.sql
@@ -28,12 +28,12 @@ set search_path = faers;
 drop table if exists standard_drug_outcome_statistics;
 create table standard_drug_outcome_statistics as
 select 
-drug_concept_id, outcome_concept_id, 
+drug_concept_id, outcome_concept_id, cast(null as integer) as snomed_outcome_concept_id,
 count_a as case_count,
-(count_a / (count_a + count_b)) / (count_c / (count_c + count_d)) as prr,
-exp(ln((count_a / (count_a + count_b)) / (count_c / (count_c + count_d)))+1.96*sqrt((1.0/count_a)-(1.0/(count_a+count_b))+(1.0/count_c)+(1.0/(count_c+count_d)))) as prr_95_percent_upper_confidence_limit,
-exp(ln((count_a / (count_a + count_b)) / (count_c / (count_c + count_d)))-1.96*sqrt((1.0/count_a)-(1.0/(count_a+count_b))+(1.0/count_c)+(1.0/(count_c+count_d)))) as prr_95_percent_lower_confidence_limit,
-((count_a / count_c) / (count_b / count_d)) as ror,
-exp((ln((count_a / count_c) / (count_b / count_d))+1.96)*sqrt((1.0/count_a)+(1.0/count_b)+(1.0/count_c)+(1.0/count_d))) as ror_95_percent_upper_confidence_limit,
-exp((ln((count_a / count_c) / (count_b / count_d))-1.96)*sqrt((1.0/count_a)+(1.0/count_b)+(1.0/count_c)+(1.0/count_d))) as ror_95_percent_lower_confidence_limit
+round((count_a / (count_a + count_b)) / (count_c / (count_c + count_d)),5) as prr,
+round(exp(ln((count_a / (count_a + count_b)) / (count_c / (count_c + count_d)))+1.96*sqrt((1.0/count_a)-(1.0/(count_a+count_b))+(1.0/count_c)+(1.0/(count_c+count_d)))),5) as prr_95_percent_upper_confidence_limit,
+round(exp(ln((count_a / (count_a + count_b)) / (count_c / (count_c + count_d)))-1.96*sqrt((1.0/count_a)-(1.0/(count_a+count_b))+(1.0/count_c)+(1.0/(count_c+count_d)))),5) as prr_95_percent_lower_confidence_limit,
+round(((count_a / count_c) / (count_b / count_d)),5) as ror,
+round(exp((ln((count_a / count_c) / (count_b / count_d))+1.96)*sqrt((1.0/count_a)+(1.0/count_b)+(1.0/count_c)+(1.0/count_d))),5) as ror_95_percent_upper_confidence_limit,
+round(exp((ln((count_a / count_c) / (count_b / count_d))-1.96)*sqrt((1.0/count_a)+(1.0/count_b)+(1.0/count_c)+(1.0/count_d))),5) as ror_95_percent_lower_confidence_limit
 from standard_drug_outcome_contingency_table;
