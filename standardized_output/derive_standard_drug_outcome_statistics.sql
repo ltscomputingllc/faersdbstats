63c5dca64ed44dadd21fc34a7197eb39ce5ce696
leeevans <levans@ltscomputingllc.com>
2015-10-05 18:35:44 -0400

    fixed confidence interval calculations for ROR

--- standardized_output/derive_standard_drug_outcome_statistics.sql
+++ standardized_output/derive_standard_drug_outcome_statistics.sql
@@ -34,6 +34,6 @@ round((count_a / (count_a + count_b)) / (count_c / (count_c + count_d)),5) as pr
 round(exp(ln((count_a / (count_a + count_b)) / (count_c / (count_c + count_d)))+1.96*sqrt((1.0/count_a)-(1.0/(count_a+count_b))+(1.0/count_c)+(1.0/(count_c+count_d)))),5) as prr_95_percent_upper_confidence_limit,
 round(exp(ln((count_a / (count_a + count_b)) / (count_c / (count_c + count_d)))-1.96*sqrt((1.0/count_a)-(1.0/(count_a+count_b))+(1.0/count_c)+(1.0/(count_c+count_d)))),5) as prr_95_percent_lower_confidence_limit,
 round(((count_a / count_c) / (count_b / count_d)),5) as ror,
-round(exp((ln((count_a / count_c) / (count_b / count_d))+1.96)*sqrt((1.0/count_a)+(1.0/count_b)+(1.0/count_c)+(1.0/count_d))),5) as ror_95_percent_upper_confidence_limit,
-round(exp((ln((count_a / count_c) / (count_b / count_d))-1.96)*sqrt((1.0/count_a)+(1.0/count_b)+(1.0/count_c)+(1.0/count_d))),5) as ror_95_percent_lower_confidence_limit
+round(exp((ln((count_a / count_c) / (count_b / count_d)))+1.96*sqrt((1.0/count_a)+(1.0/count_b)+(1.0/count_c)+(1.0/count_d))),5) as ror_95_percent_upper_confidence_limit,
+round(exp((ln((count_a / count_c) / (count_b / count_d)))-1.96*sqrt((1.0/count_a)+(1.0/count_b)+(1.0/count_c)+(1.0/count_d))),5) as ror_95_percent_lower_confidence_limit
 from standard_drug_outcome_contingency_table;
