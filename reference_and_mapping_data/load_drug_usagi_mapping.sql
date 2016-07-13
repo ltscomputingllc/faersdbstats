149369144b4115ca361f7a268a2671f56e5d30e7
Lee Evans <leeevans@users.noreply.github.com>
2016-07-13 08:01:07 -0400

    add load_drug_usagi_mapping.sql

---
+++ reference_and_mapping_data/load_drug_usagi_mapping.sql
@@ -0,0 +1,14 @@
+------------------------------
+-- Load results of USAGI manual mapping into drug_usagi_mapping table via the usagi_import table
+--
+-- First manually import the usagi mapping results into the usagi_import table using
+-- the pgadmin SQL client table import functionality and then run the below insert query
+--
+-- LTS COMPUTING LLC
+------------------------------
+insert into drug_usagi_mapping
+select a.source_code_description as drug_name_original , 
+b.concept_name, b.concept_class_id, cast(a.target_concept_id as integer) as concept_id, cast ('usagi' as text) as update_method
+from usagi_import a
+inner join cdmv5.concept b
+on cast(a.target_concept_id as integer) = b.concept_id
