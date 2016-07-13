1cddfcf08bd117f1d508f88a3316fa0d83df67b5
Lee Evans <leeevans@users.noreply.github.com>
2016-07-13 07:58:09 -0400

    update comments

--- reference_and_mapping_data/generate_drug_export_for_usagi.sql
+++ reference_and_mapping_data/generate_drug_export_for_usagi.sql
@@ -1,5 +1,7 @@
 -- This SQL generates the set of unmatched drug names with a dummy source_id and a source frequency based on frequency of cases
 -- In postgres SQL client save the results to a csv file and then import that csv file to usagi for manual code curation
+--
+-- LTS Computing LLC
 --------------------------------------------
 select row_number() over () as source_code, 
 upper(drug_name_original) as source_code_description, 
