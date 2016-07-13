dabc89ae94856601b3f79fbe9aadfcf32b3c993f
Lee Evans <leeevans@users.noreply.github.com>
2016-07-13 07:56:18 -0400

    add generate_drug_export_for_usagi.sql file

---
+++ reference_and_mapping_data/generate_drug_export_for_usagi.sql
@@ -0,0 +1,9 @@
+-- This SQL generates the set of unmatched drug names with a dummy source_id and a source frequency based on frequency of cases
+-- In postgres SQL client save the results to a csv file and then import that csv file to usagi for manual code curation
+--------------------------------------------
+select row_number() over () as source_code, 
+upper(drug_name_original) as source_code_description, 
+count(*) as frequency -- (case frequency)
+from combined_drug_mapping where concept_id is null
+group by upper(drug_name_original)
+order by count(*) desc
