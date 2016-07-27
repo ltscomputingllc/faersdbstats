c7da699964c438e458f4e3695335ef126625061e
Lee Evans <leeevans@users.noreply.github.com>
2016-07-27 17:18:20 -0400

    added the 14Q2, 14Q3 and 14Q4 loads

--- load_data_files_from_website/create_current_all_outc_data_file_with_filename_column.sh
+++ load_data_files_from_website/create_current_all_outc_data_file_with_filename_column.sh
@@ -37,5 +37,20 @@ thefilename="${thefilenamenoprefix}.txt"
 # remove windows carriage return, remove the header line and add the filename as the last column on each line
 sed 's/\r$//' "${thefilename}"| sed '1,1d' | sed "1,$ s/$/\$${thefilename}/" >"${thefilenamenoprefix}_with_filename.txt"
 
+thefilenamenoprefix=OUTC14Q2
+thefilename="${thefilenamenoprefix}.txt"
+# remove windows carriage return, remove the header line and add the filename as the last column on each line
+sed 's/\r$//' "${thefilename}"| sed '1,1d' | sed "1,$ s/$/\$${thefilename}/" >"${thefilenamenoprefix}_with_filename.txt"
+
+thefilenamenoprefix=OUTC14Q3
+thefilename="${thefilenamenoprefix}.txt"
+# remove windows carriage return, remove the header line and add the filename as the last column on each line
+sed 's/\r$//' "${thefilename}"| sed '1,1d' | sed "1,$ s/$/\$${thefilename}/" >"${thefilenamenoprefix}_with_filename.txt"
+
+thefilenamenoprefix=OUTC14Q4
+thefilename="${thefilenamenoprefix}.txt"
+# remove windows carriage return, remove the header line and add the filename as the last column on each line
+sed 's/\r$//' "${thefilename}"| sed '1,1d' | sed "1,$ s/$/\$${thefilename}/" >"${thefilenamenoprefix}_with_filename.txt"
+
 # concatenate all the outcome files with filenames together into a single file for loading
 cat outc12q4_with_filename.txt OUTC*_with_filename.txt  > all_outc_data_with_filename.txt
