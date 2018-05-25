b21d4778ab5b1ff19185027ba6a124c1e5524926
Lee Evans <leeevans@users.noreply.github.com>
2018-05-25 19:31:02 +0000

    add latest 2017 source files

--- load_data_files_from_website/create_current_all_indi_data_file_with_filename_column.sh
+++ load_data_files_from_website/create_current_all_indi_data_file_with_filename_column.sh
@@ -5,7 +5,7 @@
 # LTS Computing LLC
 ##########################################################################
 
-# load the indication files: indi12q4.txt INDI13Q1.txt  INDI13Q2.txt  INDI13Q3.txt  INDI13Q4.txt  INDI14Q1.txt  INDI14Q2.txt  INDI14Q3.txt  INDI14Q4.txt
+# load the indication files
 
 thefilenamenoprefix=indi12q4
 thefilename="${thefilenamenoprefix}.txt"
@@ -93,6 +93,21 @@ thefilename="${thefilenamenoprefix}.txt"
 sed 's/\r$//' "${thefilename}"| sed '1,1d' | sed "1,$ s/$/\$${thefilename}/" >"${thefilenamenoprefix}_with_filename.txt"
 
 thefilenamenoprefix=INDI17Q1
+thefilename="${thefilenamenoprefix}.txt"
+# remove windows carriage return, remove the header line and add the filename as the last column on each line
+sed 's/\r$//' "${thefilename}"| sed '1,1d' | sed "1,$ s/$/\$${thefilename}/" >"${thefilenamenoprefix}_with_filename.txt"
+
+thefilenamenoprefix=INDI17Q2
+thefilename="${thefilenamenoprefix}.txt"
+# remove windows carriage return, remove the header line and add the filename as the last column on each line
+sed 's/\r$//' "${thefilename}"| sed '1,1d' | sed "1,$ s/$/\$${thefilename}/" >"${thefilenamenoprefix}_with_filename.txt"
+
+thefilenamenoprefix=INDI17Q3
+thefilename="${thefilenamenoprefix}.txt"
+# remove windows carriage return, remove the header line and add the filename as the last column on each line
+sed 's/\r$//' "${thefilename}"| sed '1,1d' | sed "1,$ s/$/\$${thefilename}/" >"${thefilenamenoprefix}_with_filename.txt"
+
+thefilenamenoprefix=INDI17Q4
 thefilename="${thefilenamenoprefix}.txt"
 # remove windows carriage return, remove the header line and add the filename as the last column on each line
 sed 's/\r$//' "${thefilename}"| sed '1,1d' | sed "1,$ s/$/\$${thefilename}/" >"${thefilenamenoprefix}_with_filename.txt"
